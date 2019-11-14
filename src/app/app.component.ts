4224d20c921d6917be4b2092c467cbe1736ae40c
Nicolás Alani <32044782+PsHye@users.noreply.github.com>
2019-11-13 21:50:27 -0300

    Implentacion de Metodo
    Prueba con la implementación de métodos y cambio dinámico de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'angular-material-template';
+export class AppComponent implements OnInit{
+
+    public edited;
+    
+    ngOnInit(){
+        this.edited = false;
+    } 
+
+    TestingFunction(){
+        this.edited = !this.edited;
+        alert(this.edited)
+    }
 }
